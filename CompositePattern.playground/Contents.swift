7a7cc71b3503b9546e59fe5769846e7d9ae4a56e
ChungMai <macmini@MacMinis-Mac-mini.local>
2016-09-16 18:16:32 +0700

    Updated

--- CompositePattern.playground/Contents.swift
+++ CompositePattern.playground/Contents.swift
@@ -2,4 +2,13 @@
 
 import UIKit
 
-var str = "Hello, playground"
+class VODComponent{
+    func add(vodComponent:VODComponent){
+        assert(false, "This method is not supported")
+    }
+}
+
+
+let a = VODComponent()
+a.add(vodComponent: VODComponent())
+
