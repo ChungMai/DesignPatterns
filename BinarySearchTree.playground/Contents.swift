2780fc68a3d06829dcc37a982ce7c30fd28a1f12
ChungMai <macmini@MacMinis-Mac-mini.local>
2016-09-21 18:16:30 +0700

    Updated

--- BinarySearchTree.playground/Contents.swift
+++ BinarySearchTree.playground/Contents.swift
@@ -24,9 +24,10 @@ public class AVLTree<T:Comparable>{
                 leftChild.key = key
                 leftChild.height = 0
                 self.left = leftChild
-                print("left node:",self.left?.key)
+                //print("left node:",self.left?.key)
             }
             self.setNodeHeight()
+            isValidAVLTree()
         }
         
         if key > self.key!{
@@ -38,11 +39,55 @@ public class AVLTree<T:Comparable>{
                 rightChild.key = key
                 rightChild.height = 0
                 self.right = rightChild
-                print("right node:", self.right?.key)
+                //print("right node:", self.right?.key)
             }
             
             self.setNodeHeight()
+            isValidAVLTree()
         }
+        
+        
+    }
+    
+    func addNodeMode(key:T){
+        if self.key == nil{
+            self.key = key
+            self.height = 0
+            return
+        }
+        
+        if(key <= self.key!){
+            if self.left != nil{
+                left?.addNode(key: key)
+            }
+            else{
+                let leftChild : AVLTree = AVLTree()
+                leftChild.key = key
+                leftChild.height = 0
+                self.left = leftChild
+                //print("left node:",self.left?.key)
+            }
+            self.setNodeHeight()
+            isValidAVLTree()
+        }
+        
+        if key > self.key!{
+            if self.right != nil{
+                right?.addNode(key: key)
+            }
+            else{
+                let rightChild : AVLTree = AVLTree()
+                rightChild.key = key
+                rightChild.height = 0
+                self.right = rightChild
+                //print("right node:", self.right?.key)
+            }
+            
+            self.setNodeHeight()
+            isValidAVLTree()
+        }
+        
+        
     }
     
     func getNodeHeigh(aNode : AVLTree!) -> Int{
@@ -79,15 +124,107 @@ public class AVLTree<T:Comparable>{
         return false
     }
     
+    func isValidAVLTree()->Bool?{
+    
+        //check for valid scenario
+        if (self.key == nil) {
+            print("no key provided..")
+            return false
+        }
+        
+        
+        if (self.isTreeBalanced() == true) {
+            //print("node \(self.key!) already balanced..")
+            return true
+        }
+        
+        let childToUse : AVLTree = AVLTree()
+        childToUse.height = 0
+        childToUse.key = self.key
+        print("Change root:", self.key)
+        
+        if(getNodeHeigh(aNode: self.left) - getNodeHeigh(aNode: self.right) > 1){
+            self.key = self.left?.key
+            let misKey = self.left?.right?.key
+            
+            self.height = getNodeHeigh(aNode: self.left)
+        
+            childToUse.right =  self.right
+            self.right = childToUse
+            
+            
+            self.left = self.left?.left
+            self.left?.height = 0
+            
+            if misKey != nil{
+                addNode(key: misKey!)
+            }
+            
+            
+//            print("Node ", self.key)
+//            print("Left Node ", self.left?.key)
+//            print("Right Node ", self.right?.key)
+            return true
+        }
+        
+        if(getNodeHeigh(aNode: self.right) - getNodeHeigh(aNode: self.left) > 1){
+            self.key = self.right?.key
+            let misKey = self.right?.left?.key
+            self.height = getNodeHeigh(aNode: self.right)
+            
+            childToUse.left = self.left
+            self.left = childToUse
+            
+            self.right = self.right?.right
+            self.right?.height = 0
+            
+            if misKey != nil{
+                addNode(key: misKey!)
+            }
+//            print("Node ", self.key)
+//            print("Left Node ", self.left?.key)
+//            print("Right Node ", self.right?.key)
+            return true
+        }
+        
+        return nil
+    }
+    
+    func printNodes(aNode : AVLTree){
+        if aNode.key == nil{
+            return
+        }
+        
+        print(aNode.key)
+        
+        if(aNode.left != nil){
+            printNodes(aNode: aNode.left!)
+        }
+        
+        if(aNode.right != nil){
+            printNodes(aNode: aNode.right!)
+        }
+    }
+    
 }
 
-
-let numberList : Array<Int> =  [8, 2, 10, 9, 11, 1, 7]
+// 19, 17, 18,14,12
+let numberList : Array<Int> =  [29,26,23, 19,17, 18, 14, 12, 6, 4,2,1]
 var root = AVLTree<Int>()
 
 for number in numberList{
     root.addNode(key: number)
 }
 
-print(root.right?.left?.key)
+root.isValidAVLTree()
+
+print("Root:",root.key)
+
+print("Root Left:",root.left?.right?.right?.key)
+
+print("Right Node")
+root.printNodes(aNode: root.right!)
+
+print("Left Node")
+root.printNodes(aNode: root.left!)
 
